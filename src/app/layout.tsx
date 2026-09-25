bab0c052847234c8746788762f067e6f312b9fb2
Orbit Infotech <info@orbitinfotech.com>
2026-09-25 10:05:58 +0530

    white label

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,10 +22,10 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: {
-    default: "wacrm",
-    template: "%s — wacrm",
+    default: "WhatsApp crm",
+    template: "%s — WhatsApp crm",
   },
-  description: "Self-hostable CRM template for WhatsApp.",
+  description: "Self-hostable WhatsApp CRM.",
   robots: {
     index: false,
     follow: false,
